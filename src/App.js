14e557d79cfc6f79b6a3eb4a77bc07494d7280f3
이희수 <leeheesu@ihuisuui-MacBookAir.local>
2025-12-14 19:36:47 +0900

    로컬 스토리지, lodash.debounce, useCallback 적용

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import SideBar from "./components/SideBar";
 import MemoContainer from "./components/MemoContainer";
+import { setItem, getItem } from "./lib/storage";
+import debounce from "lodash.debounce";
+
+const debouncedSetItem = debounce(setItem, 5000);
+
 function App() {
-  const [memos, setMemos] = useState([
-    {
-      title: "memo1",
-      cont: "This is memo1",
-      credt: 1765633464448,
-      upddt: 1765633464448,
-    },
-    {
-      title: "memo2",
-      cont: "This is memo2",
-      credt: 1765633464448,
-      upddt: 1765633464448,
-    },
-  ]);
+  const [memos, setMemos] = useState(getItem("memo") || []);
 
   const [selectedMemoIndex, setSelectMemoIndex] = useState(0);
 
-  const setMemo = (newMemo) => {
-    const newMemos = [...memos]; // 불변성 확보를 위해 memos 원본을 사용하지 않음.
-    newMemos[selectedMemoIndex] = newMemo;
-    setMemos(newMemos); // setMemos(memos) 로는 갱신안됨, reference상 같은 오브젝트(같은 주소)라고 생각하기 때문에 렌더링 안됨
-  };
+  const setMemo = useCallback(
+    (newMemo) => {
+      /**
+      const newMemos = [...memos]; // 불변성 확보를 위해 memos 원본을 사용하지 않음.
+      newMemos[selectedMemoIndex] = newMemo;
+      setMemos(newMemos); // setMemos(memos) 로는 갱신안됨, reference상 같은 오브젝트(같은 주소)라고 생각하기 때문에 렌더링 안됨
+      debouncedSetItem("memo", newMemos);
+      */
+      // useCallback 이 memos 에 의존하는걸 막기위해 아래와 같이 리팩토링
+
+      setMemos((memos) => {
+        const newMemos = [...memos]; // 불변성 확보를 위해 memos 원본을 사용하지 않음.
+        newMemos[selectedMemoIndex] = newMemo;
+        debouncedSetItem("memo", newMemos);
+        return newMemos;
+      });
+    },
+    [selectedMemoIndex]
+  );
 
   const addMemo = () => {
     const now = new Date().getTime();
-    setMemos([
+    const newMemos = [
       ...memos,
       {
         title: "Untitled",
@@ -36,20 +42,29 @@ function App() {
         credt: now,
         upddt: now,
       },
-    ]);
-
-    setSelectMemoIndex(memos.length);
-  };
-
-  const deleteMemo = (index) => {
-    const newMemos = [...memos];
-    newMemos.splice(index, 1);
+    ];
     setMemos(newMemos);
 
-    if (index == selectedMemoIndex) {
-      setSelectMemoIndex(0);
-    }
+    setSelectMemoIndex(memos.length);
+    debouncedSetItem("memo", newMemos);
   };
+
+  const deleteMemo = useCallback(
+    (index) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
+        newMemos.splice(index, 1);
+
+        debouncedSetItem("memo", newMemos);
+        return newMemos;
+      });
+
+      if (index == selectedMemoIndex) {
+        setSelectMemoIndex(0);
+      }
+    },
+    [selectedMemoIndex]
+  );
 
   return (
     <div className="App">
